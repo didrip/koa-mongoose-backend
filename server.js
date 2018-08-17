1a5d82b52b8603e6e9c166f2cd403c1e25ed7dff
monkeydri <dri@openmail.cc>
2018-08-17 10:58:52 +0200

    todos starting base

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const Koa = require('koa')
-const Router = require('koa-router')
-const Logger = require('koa-logger')
-const Cors = require('@koa/cors')
-const BodyParser = require('koa-bodyparser')
-const Helmet = require('koa-helmet')
-const respond = require('koa-respond')
+const Koa = require('koa');
+const Router = require('koa-router');
+const Logger = require('koa-logger');
+const Cors = require('@koa/cors');
+const BodyParser = require('koa-bodyparser');
+const Helmet = require('koa-helmet');
+const respond = require('koa-respond');
+const mongoose = require('mongoose');
 
 const app = new Koa()
 const router = new Router()
@@ -30,6 +31,16 @@ app.use(respond())
 // API routes
 require('./routes')(router)
 app.use(router.routes())
+
+// serve compiled front-end
+app.use(require('koa-static')('./build'))
+
 app.use(router.allowedMethods())
+
+// remote MongoDB
+// mongoose.connect('mongodb://username:pwd@ds247141.mlab.com:47141/database-name')
+
+// remote local mongoDB (mongodb-runner)
+mongoose.connect('mongodb://localhost:27017/test')
 
 module.exports = app
